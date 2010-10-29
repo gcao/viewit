894863b670b65dfe085319fc729cce4a65ac3c8b
Guoliang Cao <gcao99@gmail.com>
2010-10-29 17:44:10 -0400

    Create revisions from js and data model

--- experimental/test-load.js
+++ experimental/test-load.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
-  $('#forks ul').html(Jaml.render('fork', data.current_gist.forks));
+  simpleUpdate('#forks ul', 'fork', data.current_gist.forks);
+  simpleUpdate('#revisions ul', 'revision', data.current_gist.revisions);
 });
+
+function simpleUpdate(path, template, data) {
+  $(path).html(Jaml.render(template, data));
+}
 
 Jaml.register("fork", function(fork){
   li(
@@ -16,3 +21,29 @@ Jaml.register("fork", function(fork){
     )
   );
 });
+
+Jaml.register("revision", function(revision){
+  li({cls: "other"},
+    renderChanges(revision.changes_abbr),
+    a({cls: "id", href: revision.view_url}, revision._id),
+    a({cls: "author", href: revision.owner.view_url}, revision.owner.username),
+    span({cls: "date"},
+      abbr({cls: "relatize relatized", title: revision.created_at}, revision.created_at_alias)
+    )
+  );
+});
+
+// changes is a String like '++--**'
+function renderChanges(changes){
+  var s = "";
+  for(var i=0; i<changes.length; i++) {
+    var change = changes.charAt(i);
+    if (change == '+')
+      s += "<img alt='' src='test_files/dot_green.png' />";
+    else if (change == '-')
+      s += "<img alt='' src='test_files/dot_red.png' />";
+    else if (change == '*')
+      s += "<img alt='' src='test_files/dot_gray.png' />";
+  }
+  return s;
+}
